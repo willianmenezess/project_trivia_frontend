9a455c8a227a6475aee5e8df320bde27afdc0204
MarcosRanauro <marcosranauro@hotmail.com>
2023-04-24 23:21:59 -0300

    Feat: Resolve requisito 18
     Criado um botão para ir ao início na página de ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
+  handleLoginPage = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <h2 data-testid="ranking-title">Ranking</h2>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handleLoginPage }
+        >
+          Voltar ao Login
+
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
